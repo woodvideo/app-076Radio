e70eb85aaa977789add4aa6a6a334dc04f064a3b
woodvideo <info@woodvideo.nl>
2017-04-13 15:29:59 +0200

    Player ajustments

--- www/scripts/wc-nowplaying.js
+++ www/scripts/wc-nowplaying.js
@@ -2,139 +2,161 @@ function startTime() {
 		var Datum = new Date(); 
 		var dag = Datum.getDay();  
 		var uur = Datum.getHours();
-        var progNu = document.getElementById('progTitel')
-        var progStraks = document.getElementById('progLater')
-		var ProgImg = document.getElementById('progFoto')
-		var ProgFoto = document.getElementById('progFoto')
-		var progUrl = document.getElementById('progUrl')
+        var progNu = document.getElementById('progTitel');
+        var progStraks = document.getElementById('progLater');
+		var ProgImg = document.getElementById('progFoto');
+		var ProgFoto = document.getElementById('progFoto');
+		var progUrl = document.getElementById('progUrl');
+		var progPres= document.getElementById('progPres');
+		var progLater = "STRAKS: "+progStraks;
 
 
         	
 		if (dag==6 && uur >= 8 && uur < 10) { 
 			progNu.innerHTML = "Non-stop hits"; 
-            progStraks.innerHTML= "Het besten van Antoon van der Westen";
-			progFoto.src="logos/ns.jpg";} 
+            progStraks.innerHTML="Straks: Het besten van Antoon van der Westen";
+			progFoto.src="logos/ns.jpg";
+			progPres.innerHTML="076 Radio mixmachine";} 
 		else 
 
 		if (dag==0 && uur >= 8 && uur < 10) { 
 			progNu.innerHTML = "Non-stop hits"; 
-            progStraks.innerHTML= "Het besten van Antoon van der Westen";
-			progFoto.src="logos/ns.jpg";} 
+            progStraks.innerHTML="Straks: Het besten van Antoon van der Westen";
+			progFoto.src="logos/ns.jpg";
+			progPres.innerHTML="076 Radio mixmachine";} 
 		else 			
 			
 		if (dag==6 && uur >= 10 && uur < 11) { 
 			progNu.innerHTML = "Het besten van Antoon van der Westen"; 
-            progStraks.innerHTML= "DE HITS GEZONGEN MET van Dongen";
-			progFoto.src="logos/ktmav.png";} 
+            progStraks.innerHTML="Straks: DE HITS GEZONGEN MET van Dongen";
+			progFoto.src="logos/ktmav.png";
+			progPres.innerHTML="Antoon van der Westen";} 
 		else  
 		
 		if (dag==0 && uur >= 10 && uur < 11) { 
 			progNu.innerHTML = "Het besten van Antoon van der Westen"; 
-            progStraks.innerHTML= "DE HITS GEZONGEN MET van Dongen";
-			progFoto.src="logos/ktmav.png";} 
+            progStraks.innerHTML="Straks: DE HITS GEZONGEN MET van Dongen";
+			progFoto.src="logos/ktmav.png";
+			progPres.innerHTML="Antoon van der Westen";} 
 		else  
 	
 		if (dag==6 && uur >= 11 && uur < 12) { 
 			progNu.innerHTML = "DE HITS GEZONGEN MET van Dongen"; 
-            progStraks.innerHTML="Typisch Sander";
-			progFoto.src="logos/hgmv.jpg";} 
+            progStraks.innerHTML="Straks: Typisch Sander";
+			progFoto.src="logos/hgmv.jpg";
+			progPres.innerHTML="Voornaam van Dongen";} 
 		else  
 			
 		if (dag==0 && uur >= 11 && uur < 12) { 
 			progNu.innerHTML = "DE HITS GEZONGEN MET van Dongen"; 
-            progStraks.innerHTML="Typisch Sander";
-			progFoto.src="logos/hgmv.jpg";} 
+            progStraks.innerHTML="Straks: Typisch Sander";
+			progFoto.src="logos/hgmv.jpg";
+			progPres.innerHTML="Voornaam van Dongen";} 
 		else  
 
 		if (dag==6 && uur >= 12 && uur < 13) { 
 			progNu.innerHTML = "Typisch Sander";
-			progStraks.innerHTML="Typisch Sander non-stop";
-			progFoto.src="logos/ts.jpg";}
+			progStraks.innerHTML="Straks: Typisch Sander non-stop";
+			progFoto.src="logos/ts.jpg";
+			progPres.innerHTML="Sander Waasdorp";}
 		else  
 			
 		if (dag==0 && uur >= 12 && uur < 13) { 
 			progNu.innerHTML = "Typisch Sander";
-			progStraks.innerHTML="Typisch Sander non-stop";
-			progFoto.src="logos/ts.jpg";}
+			progStraks.innerHTML="Straks: Typisch Sander non-stop";
+			progFoto.src="logos/ts.jpg";
+			progPres.innerHTML="Sander Waasdorp";}
 		else  
 
 		if (dag==6 && uur >= 13 && uur < 14) { 
 			progNu.innerHTML = "Typisch Sander non-stop"; 
-			progStraks.innerHTML="NAC Praat Herhaling";
-			progFoto.src="logos/np.jpg";} 
+			progStraks.innerHTML="Straks: NAC Praat Herhaling";
+			progFoto.src="logos/np.jpg";
+			progPres.innerHTML="Sander Waasdorp";} 
 		else  
 		
 		if (dag==0 && uur >= 13 && uur < 14) { 
 			progNu.innerHTML = "Typisch Sander non-stop"; 
-			progStraks.innerHTML="Weekendkr8 Dance Classics"; 
-			progFoto.src="logos/teamnl.jpg";} 
+			progStraks.innerHTML="Straks: Weekendkr8 Dance Classics"; 
+			progFoto.src="logos/teamnl.jpg";
+			progPres.innerHTML="Sander Waasdorp";} 
 		else  
 		
 		if (dag==6 && uur >= 14 && uur < 16) { 
 			progNu.innerHTML = "NAC Praat Herhaling"; 
-			progStraks.innerHTML="Koen in the Afternoon";
-			progFoto.src="logos/t80.jpg";} 
+			progStraks.innerHTML="Straks: Koen in the Afternoon";
+			progFoto.src="logos/t80.jpg";
+			progPres.innerHTML="Fanzine De Rat";} 
 		else  
 
 		if (dag==0 && uur >= 14 && uur < 16) { 
 			progNu.innerHTML = "Weekendkr8 Dance Classics"; 
-			progStraks.innerHTML="Koen in the Afternoon";
-			progFoto.src="logos/t80.jpg";} 
+			progStraks.innerHTML="Straks: Koen in the Afternoon";
+			progFoto.src="logos/t80.jpg";
+			progPres.innerHTML="Jan Krijnen";} 
 		else  			
 
 		if (dag==6 && uur >= 16 && uur < 17) { 
 			progNu.innerHTML = "Koen in the Afternoon"; 
-			progStraks.innerHTML="Nonstop top 40 hits";
+			progStraks.innerHTML="Straks: Nonstop top 40 hits";
 			progFoto.src="logos/kita.jpg";
 			progUrl.href="p-koenvdh.html";
-			progUrl.innerHTML = "meer informatie"} 			
+			progUrl.innerHTML = "meer informatie";			
+			progPres.innerHTML="Koen van der Horst";} 			
 		else  
 			
 		if (dag==0 && uur >= 16 && uur < 17) { 
 			progNu.innerHTML = "Koen in the Afternoon"; 
-			progStraks.innerHTML="Nonstop top 40 hits";
+			progStraks.innerHTML="Straks: Nonstop top 40 hits";
 			progFoto.src="logos/kita.jpg";
-			progUrl.href="p-koenvdh.html"} 			
+			progUrl.href="p-koenvdh.html";
+			progPres.innerHTML="Koen van der Horst";} 			
 		else 		
 
 		if (dag==6 && uur >= 17 && uur < 19) { 
 			progNu.innerHTML = "Nonstop top 40 hits"; 
-			progStraks.innerHTML="Team NL Voor het stappen gaan";
-			progFoto.src="logos/ns40.png";} 
+			progStraks.innerHTML="Straks: Team NL Voor het stappen gaan";
+			progFoto.src="logos/ns40.png";
+			progPres.innerHTML="Nienke en Lobke Krijnen";} 
 		else  
 
 		if (dag==6 && uur >= 19 && uur < 21) { 
 			progNu.innerHTML = "Team NL Voor het stappen gaan"; 
-			progStraks.innerHTML="Weekeind Kr8 Dance Classics";
-			progFoto.src="logos/vhsg.jpg";} 
+			progStraks.innerHTML="Straks: Weekeind Kr8 Dance Classics";
+			progFoto.src="logos/vhsg.jpg";
+			progPres.innerHTML="Nienke en Lobke Krijnen";} 
 		else  
 
 		if (dag==6 && uur >= 22 && uur < 24) { 
 			progNu.innerHTML = "Weekeind Kr8 Dance Classics"; 
-			progStraks.innerHTML="Non-stop hits";
-			progFoto.src="logos/wkdc.jpg";} 
+			progStraks.innerHTML="Straks: Non-stop hits";
+			progFoto.src="logos/wkdc.jpg";
+			progPres.innerHTML="Jan Krijnen";} 
 		else  
 
 		if (dag==0 && uur >= 17 && uur < 18) { 
 			progNu.innerHTML = "Nonstop top 40 hits"; 
-			progStraks.innerHTML="Hits From The Heartland Camiel Janszen";
-			progFoto.src="logos/ns40.jpg";} 
+			progStraks.innerHTML="Straks: Hits From The Heartland Camiel Janszen";
+			progFoto.src="logos/ns40.jpg";
+			progPres.innerHTML="076 Radio mixmachine";} 
 		else  
 
 		if (dag==0 && uur >= 18 && uur < 20) { 
 			progNu.innerHTML = "Hits From The Heartland Camiel Janszen"; 
-			progStraks.innerHTML="The Symfo Experience met Ad van Loon";
-			progFoto.src="logos/hfthcj.jpg";} 
+			progStraks.innerHTML="Straks: The Symfo Experience met Ad van Loon";
+			progFoto.src="logos/hfthcj.jpg";
+			progPres.innerHTML="Camiel Janszen";} 
 		else  
 
 		if (dag==0 && uur >= 20 && uur < 22) { 
 			progNu.innerHTML = "The Symfo Experience met Ad van Loon"; 
-			progStraks.innerHTML="Non-stop hits";
-			progFoto.src="logos/tse.jpg";} 
+			progStraks.innerHTML="Straks: Non-stop hits";
+			progFoto.src="logos/tse.jpg";
+			progPres.innerHTML="Ad van Loon";} 
 		
 		else  {		 
 			progNu.innerHTML = "Non-stop hits"; 
-			progStraks.innerHTML="Non-stop hits";
-			progFoto.src="logos/ns.jpg";}
+			progFoto.src="logos/ns.jpg";
+			progPres.innerHTML="076 Radio mixmachine";}
         var t = setTimeout(startTime, 500);    
 }
